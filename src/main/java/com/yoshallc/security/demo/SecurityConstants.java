6a2e418a196605f556c953528e60070f9ee68bbf
palnil <palnil@gmail.com>
2018-11-12 21:28:00 -0500

    Initial Check in

--- src/main/java/com/yoshallc/security/demo/SecurityConstants.java
+++ src/main/java/com/yoshallc/security/demo/SecurityConstants.java
@@ -2,8 +2,8 @@ package com.yoshallc.security.demo;
 
 
 public class SecurityConstants {
-    private static final String SECRET = "secret";
-    private static final long EXPIRATION_TIME = 864_000_000; // 10 days
-    private static final String TOKEN_PREFIX = "Bearer ";
-    private static final String HEADER_STRING = "Authorization";
+    public static final String SECRET = "secret";
+    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
 }
